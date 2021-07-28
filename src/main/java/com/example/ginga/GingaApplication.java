d4869423d8034c69823c18857613e27c0a978c55
mikai <ricardokaka0@sina.com>
2021-07-28 09:24:19 +0800

    初始化代码库

---
+++ src/main/java/com/example/ginga/GingaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ginga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GingaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GingaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ginga/GingaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ginga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GingaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
