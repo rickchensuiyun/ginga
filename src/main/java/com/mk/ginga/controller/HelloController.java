1d4c15b693738d6b8392993c763d3a2e410002f6
mikai <ricardokaka0@sina.com>
2021-08-02 14:47:40 +0800

    忽略配置文件，避免将关键配置提交到github

--- src/main/java/com/mk/ginga/controller/HelloController.java
+++ src/main/java/com/mk/ginga/controller/HelloController.java
@@ -4,8 +4,6 @@ import com.mk.ginga.service.HelloService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +19,9 @@ public class HelloController {
     public String hello() {
 
         log.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>hello");
+
 //        service.set("num1","10001");
 //        service.set("msg1","hello service ");
-
         return "hello ginga!";
     }
 
